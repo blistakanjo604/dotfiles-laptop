e288bac9afbbe1530afafb24b7c125010ca18563
Kyle Javier <43.splash@gmail.com>
2022-05-03 00:56:13 -0400

    modified startup applications

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -12,7 +12,7 @@ run nitrogen --restore
 run volumeicon
 # run lxsession
 run xfce4-volumed-pulse
-#run /usr/lib/gsd-datetime
+run /usr/lib/gsd-datetime
 # run /usr/lib/baloo_file
 run xfce4-clipman
 # run emojione-picker
@@ -28,7 +28,7 @@ run start-pulseaudio-x11
 # run /usr/lib/xapps/sn-watcher/xapp-sn-watcher
 run /usr/lib/at-spi-bus-launcher --launch-immediately
 #run /usr/bin/gnome-keyring-daemon --start --components=pkcs11
-#run /usr/lib/gsd-power
+run /usr/lib/gsd-power
 #run /usr/bin/gnome-keyring-daemon --start --components=secrets
 #run /usr/bin/gnome-keyring-daemon --start --components=ssh
 run ~/.config/awesome/xinput-daemon.sh
